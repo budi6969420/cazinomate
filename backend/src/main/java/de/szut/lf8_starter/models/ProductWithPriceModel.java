4677fa5f39630d43a79d3cfdbaba09a8cfb4b16f
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-03-05 10:51:04 +0100

    implemented new metadata for products

--- backend/src/main/java/de/szut/lf8_starter/models/ProductWithPriceModel.java
+++ backend/src/main/java/de/szut/lf8_starter/models/ProductWithPriceModel.java
@@ -1,22 +1,36 @@
 package de.szut.lf8_starter.models;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stripe.model.Price;
 import com.stripe.model.Product;
 import lombok.Data;
+
+import java.util.Map;
 
 @Data
 public class ProductWithPriceModel {
     private String productId;
     private String name;
-    private String description;
     private String priceId;
     private long priceAmount;
     private String currency;
+    private String amount;
+    private String isRecommended;
 
     public ProductWithPriceModel(Product product, Price price) {
         this.productId = product.getId();
         this.name = product.getName();
-        this.description = product.getDescription();
+
+        try {
+            var map = new ObjectMapper().readValue(product.getDescription(), Map.class);
+            this.amount = map.get("amount").toString();
+            this.isRecommended = map.get("isRecommended").toString();
+        }
+        catch (JsonProcessingException e) {
+            System.out.println(e.getMessage());
+        }
+
         if (price != null) {
             this.priceId = price.getId();
             this.priceAmount = price.getUnitAmount();
