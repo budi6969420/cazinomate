ea571df54585465baa70465e60d295ac3b78500d
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-03-06 11:03:14 +0100

    implemented the metadataLogic

---
+++ backend/src/main/java/de/szut/lf8_starter/game/GameService.java
@@ -0,0 +1,39 @@
+package de.szut.lf8_starter.game;
+
+import de.szut.lf8_starter.game.games.CrossyRoadBangBangGame;
+import de.szut.lf8_starter.game.games.SlotsGame;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GameService {
+
+    private final List<IGame> games = List.of(
+            new CrossyRoadBangBangGame(),
+            new SlotsGame()
+    );
+
+    public List<IGame> getGames() {
+        return this.games;
+    }
+
+    public IGame getGameById(String id) {
+        var optionalGame = games.stream().filter(x -> x.getId().equals(id)).findFirst();
+        if (optionalGame.isEmpty()) return null;
+
+        return optionalGame.get();
+    }
+
+    public GameMetadata getGameMetadataById(String id) {
+        var optionalGame = games.stream().filter(x -> x.getId().equals(id)).findFirst();
+        if (optionalGame.isEmpty()) return null;
+
+        return optionalGame.get().getMetadata();
+    }
+
+    public List<GameMetadata> getGamesMetadata() {
+        return this.games.stream().map(IGame::getMetadata).toList();
+    }
+}
