5bb86e22e6f93d29fd4779ee28dec271e329ff41
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-04-02 08:51:51 +0200

    game metadata service ficks

--- frontend/src/app/components/game-overview/game-overview.component.ts
+++ frontend/src/app/components/game-overview/game-overview.component.ts
@@ -16,37 +16,21 @@ import { Subscription } from 'rxjs';
   templateUrl: './game-overview.component.html',
   styleUrl: './game-overview.component.scss'
 })
-export class GameOverviewComponent implements OnInit, OnDestroy {
+export class GameOverviewComponent {
   @ViewChild("searchInput") searchInput!: ElementRef;
-  games: GameMetadata[] = [];
   filteredGames: GameMetadata[] = [];
-  private gameSubscription: Subscription | undefined;
 
-  constructor(private service: GameMetadataService) { }
-
-  ngOnInit(): void {
-    this.gameSubscription = this.service.getGameMetadatas().subscribe({
-      next: (games) => {
-        this.games = games;
-        this.filteredGames = [...this.games];
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.gameSubscription) {
-      this.gameSubscription.unsubscribe();
-    }
+  constructor(private gameMetadataService: GameMetadataService) {
   }
 
   updateFilteredGames(): void {
     const searchTerm = this.searchInput.nativeElement.value.toLowerCase();
     if (searchTerm) {
-      this.filteredGames = this.games.filter(game =>
+      this.filteredGames = this.gameMetadataService.getGameMetadatas().filter(game =>
         game.title.toLowerCase().includes(searchTerm)
       );
     } else {
-      this.filteredGames = [...this.games];
+      this.filteredGames = [...this.gameMetadataService.getGameMetadatas()];
     }
   }
 
--- frontend/src/app/services/game-metadata.service.ts
+++ frontend/src/app/services/game-metadata.service.ts
@@ -9,31 +9,25 @@ import { tap, catchError } from 'rxjs/operators';
 })
 export class GameMetadataService {
   private apiUrl = 'http://localhost:8080/api/game';
-  private gameMetadatasSubject = new ReplaySubject<GameMetadata[]>(1);
-  gameMetadatas$ = this.gameMetadatasSubject.asObservable();
-  private hasFetched = false;
+  gameMetadatas: GameMetadata[] = [];
+  gameMetadatasFallback: GameMetadata[] = [
+    {"id":"39c63177-b7ad-478b-a009-69b8fa043e6f","title":"Crossy Road Gang! Bang!","previewImageUrl":"https://static.wikia.nocookie.net/crossyroad/images/7/73/CrossyRoad_Chicken_x_AmongUs_RedCrewmate.png/revision/latest?cb=20240924183713", "previewHexColor": "#FFF"},
+    {"id":"92ed9e52-afd8-49a5-8b09-d7a049783725","title":"The Lucky Crewmate","previewImageUrl":"https://cdn.vectorstock.com/i/500p/28/62/casino-slot-machine-777-jackpot-winning-game-vector-23732862.jpg", "previewHexColor": "#FFF"}
+  ];
 
   constructor(private http: HttpClient) {
-    if (!this.hasFetched) {
-      this.fetchGameMetadatas();
-      this.hasFetched = true;
-    }
-  }
-
-  private fetchGameMetadatas(): void {
     this.http.get<GameMetadata[]>(this.apiUrl).pipe(
       tap(games => {
-        this.gameMetadatasSubject.next(games);
+        this.gameMetadatas = games;
       }),
-      catchError(() => {
-        const fallbackData: GameMetadata[] = [{"id":"39c63177-b7ad-478b-a009-69b8fa043e6f","title":"Crossy Road Gang! Bang!","previewImageUrl":"https://static.wikia.nocookie.net/crossyroad/images/7/73/CrossyRoad_Chicken_x_AmongUs_RedCrewmate.png/revision/latest?cb=20240924183713", "previewHexColor": "#FFF"},{"id":"92ed9e52-afd8-49a5-8b09-d7a049783725","title":"The Lucky Crewmate","previewImageUrl":"https://cdn.vectorstock.com/i/500p/28/62/casino-slot-machine-777-jackpot-winning-game-vector-23732862.jpg", "previewHexColor": "#FFF"}];
-        this.gameMetadatasSubject.next(fallbackData);
-        return of(fallbackData); // Return an Observable with the fallback data so the stream doesn't break
+      catchError((error) => {
+        this.gameMetadatas = this.gameMetadatasFallback;
+        return of(this.gameMetadatasFallback);
       })
-    ).subscribe();
+    ).subscribe({});
   }
 
-  getGameMetadatas(): Observable<GameMetadata[]> {
-    return this.gameMetadatas$;
+  getGameMetadatas(): GameMetadata[] {
+    return this.gameMetadatas;
   }
 }
