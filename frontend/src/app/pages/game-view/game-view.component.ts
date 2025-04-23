1c0505ab9f812b1b07c2629f99b625a81b120368
lucafynntasto <luca.tasto@neuland-bfi.de>
2025-04-23 12:21:36 +0200

    route

---
+++ frontend/src/app/pages/game-view/game-view.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'app-game-view',
+  imports: [],
+  templateUrl: './game-view.component.html',
+  styleUrl: './game-view.component.scss'
+})
+export class GameViewComponent implements OnInit {
+  protected gameId!: string;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.gameId = <string> this.route.snapshot.paramMap.get('gameId')
+  }
+}
