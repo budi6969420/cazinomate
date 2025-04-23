f2440ac8f1c9457d1c66e238cf7de88dcc343116
Nick Lehmkuhl <nick.l5@schule.bremen.de>
2025-04-23 09:04:43 +0200

    implemented shop

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {authGuard} from "./auth.guard";
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
 import {HomeComponent} from "./pages/home/home.component";
 import {SettingsComponent} from "./pages/settings/settings.component";
 import {ShopComponent} from "./pages/shop/shop.component";
