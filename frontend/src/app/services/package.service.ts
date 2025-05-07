9cbde49f8d632c8321394ed4d959cbe237bcf2be
Nick Lehmkuhl <nick.l5@schule.bremen.de>
2025-05-07 08:18:36 +0200

    fixed recommended flag

--- frontend/src/app/services/package.service.ts
+++ frontend/src/app/services/package.service.ts
@@ -12,12 +12,12 @@ export class PackageService {
 
   public packages: Package[] = [];
   private placeholderPackages: Package[] = [
-    {name: 'test', amount: 100, priceAmount: 16999, isRecommended: true, priceId: "1", productId: "1", currency: 'EUR', imageUrl: ""},
-    {name: 'test2', amount: 500, priceAmount: 16999, isRecommended: false, priceId: "1", productId: "2", currency: 'EUR', imageUrl: ""},
-    {name: 'test3', amount: 1000, priceAmount: 16999, isRecommended: false, priceId: "1", productId: "3", currency: 'EUR', imageUrl: ""},
-    {name: 'test4', amount: 10000, priceAmount: 16999, isRecommended: false, priceId: "1", productId: "4", currency: 'EUR', imageUrl: ""},
-    {name: 'test5', amount: 25000, priceAmount: 16999, isRecommended: true, priceId: "1", productId: "5", currency: 'EUR', imageUrl: ""},
-    {name: 'test6', amount: 50000, priceAmount: 16999, isRecommended: false, priceId: "1", productId: "6", currency: 'EUR', imageUrl: ""}];
+    {name: 'test', amount: 100, priceAmount: 16999, recommended: true, priceId: "1", productId: "1", currency: 'EUR', imageUrl: ""},
+    {name: 'test2', amount: 500, priceAmount: 16999, recommended: false, priceId: "1", productId: "2", currency: 'EUR', imageUrl: ""},
+    {name: 'test3', amount: 1000, priceAmount: 16999, recommended: false, priceId: "1", productId: "3", currency: 'EUR', imageUrl: ""},
+    {name: 'test4', amount: 10000, priceAmount: 16999, recommended: false, priceId: "1", productId: "4", currency: 'EUR', imageUrl: ""},
+    {name: 'test5', amount: 25000, priceAmount: 16999, recommended: true, priceId: "1", productId: "5", currency: 'EUR', imageUrl: ""},
+    {name: 'test6', amount: 50000, priceAmount: 16999, recommended: false, priceId: "1", productId: "6", currency: 'EUR', imageUrl: ""}];
 
 
   private apiUrl: string = "http://localhost:8080/api/product";
