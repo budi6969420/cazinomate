1dca15cef2080f95bbec4406fa86bc9b31a63b92
Nick Lehmkuhl <nick.l5@schule.bremen.de>
2025-05-08 13:15:14 +0200

    added warning, disabled packages when user is not logged in

--- frontend/src/app/pages/shop/shop.component.ts
+++ frontend/src/app/pages/shop/shop.component.ts
@@ -1,8 +1,10 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PackageComponent} from "../../components/package/package.component";
 import {PackageService} from "../../services/package.service";
 import {BuyConfirmationModalComponent} from "../../components/buy-confirmation-modal/buy-confirmation-modal.component";
 import {Package} from "../../models/package";
+import {UserService} from "../../services/user.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-shop',
@@ -14,10 +16,18 @@ import {Package} from "../../models/package";
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
   protected selectedPackage: Package | null = null;
-  constructor(protected packageService: PackageService) {
+  public user: User | null = null;
+
+  constructor(protected packageService: PackageService, private userService: UserService) {
   }
+
+  ngOnInit(): void {
+    this.userService.getUserInfo().subscribe((userData) => {
+      this.user = userData;
+    });
+    }
 
   handlePackageClick($event: Package) {
     this.selectedPackage = $event;
