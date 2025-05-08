ef455a18b27173819a02a8036eb7fb2d2720470e
Nick Lehmkuhl <nick.l5@schule.bremen.de>
2025-05-08 13:15:14 +0200

    added warning, disabled packages when user is not logged in

--- frontend/src/app/components/package/package.component.ts
+++ frontend/src/app/components/package/package.component.ts
@@ -1,20 +1,22 @@
 import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {Package} from "../../models/package";
 import {DecimalPipe, NgClass, NgForOf} from "@angular/common";
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-package',
   standalone: true,
   imports: [
-    NgClass,
     NgForOf,
-    DecimalPipe
+    DecimalPipe,
+    NgClass
   ],
   templateUrl: './package.component.html',
   styleUrl: './package.component.scss'
 })
 export class PackageComponent implements OnInit {
   @Input() package!: Package;
+  @Input() user: User | null = null;
   @Output() packageClicked = new EventEmitter<Package>();
 
   ShowConfirmationModal() {
@@ -27,5 +29,22 @@ export class PackageComponent implements OnInit {
     const cols = 8;
     const rows = 14;
     this.tiles = new Array(cols * rows);
+  }
+
+  getCardClass() : string {
+    if (this.package.recommended)
+    {
+      if (this.user == null) {
+        return 'card-recommended';
+      }
+
+      return 'card-recommended active';
+    }
+
+    if (this.user == null) {
+      return 'card';
+    }
+
+    return 'card active';
   }
 }
