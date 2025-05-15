3b8c29dbe9e5a849348aee7d4446352e983971b7
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-05-15 08:40:31 +0200

    added balance to navbar

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {NewPassword} from "../models/newPassword";
 import {NewUsername} from "../models/newUsername";
-import {User} from "../models/user";
-import {Observable} from "rxjs";
+import {user} from "../models/user";
+import {Observable, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +11,29 @@ import {Observable} from "rxjs";
 export class UserService {
 
   private apiUrl = 'http://localhost:8080/api/user';
+  public myUser: user | null = null;
+  public myBalance: number = 0;
 
   constructor(private http: HttpClient) {}
 
-  getUserInfo(): Observable<User>{
-    return this.http.get<User>(this.apiUrl + "/self");
+  updateSelfUserInfo(): Observable<user>{
+    return this.http.get<user>(this.apiUrl + "/self").pipe(
+      tap(user => this.myUser = user),
+    );
   }
-  changeUsername(username: NewUsername): Observable<User>{
-    return this.http.put<User>(this.apiUrl + "/username", username);
+  updateSelfBalance(): Observable<number> {
+    return this.http.get<number>(`${this.apiUrl}/self/balance`).pipe(
+      tap(balance => this.myBalance = balance)
+    );
   }
-  changePassword(userPassword: NewPassword): Observable<User>{
-    return this.http.put<User>(this.apiUrl + "/password", userPassword);
+  changeUsername(username: NewUsername): Observable<user>{
+    return this.http.put<user>(this.apiUrl + "/username", username);
+  }
+  changePassword(userPassword: NewPassword): Observable<user>{
+    return this.http.put<user>(this.apiUrl + "/password", userPassword);
+  }
+  onLogout() {
+    this.myUser = null;
+    this.myBalance = 0;
   }
 }
