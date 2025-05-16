0dd49825cadf19809037f5360c2e0c78d4bda3a8
Fabian Bruens <fabian.bruens@neuland-bfi.de>
2025-05-16 15:09:12 +0200

    Footer payment providers hinzugefügt + flexwrap für theoretische tablet ansicht; backend payment provider fehlen noch

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,16 +1,20 @@
 import {Component} from '@angular/core';
 import {SponsorService} from "../../services/sponsor.service";
 import {GameMetadataService} from "../../services/game-metadata.service";
+import {PaymentProviderService} from "../../services/payment-provider.service";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
-  constructor(protected sponsorService: SponsorService, protected gameMetaDataService: GameMetadataService) {
+  constructor(protected sponsorService: SponsorService, protected gameMetaDataService: GameMetadataService, protected paymentProviderService : PaymentProviderService) {
   }
 }
