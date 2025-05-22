d1175e895913ea572d9dd5b6dc5addd5499e527c
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-05-22 10:33:50 +0200

    fixed prod links

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApiUrl: 'https://api.cazinomate.de',
-  keyCloakApiUrl: 'https://sso.cazinomate.de',
+  backendApiUrl: 'https://api.cazinomate.de/',
+  keyCloakApiUrl: 'https://sso.cazinomate.de/',
 };
