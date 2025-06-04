ea0d8ae750b7d0a42477866bff3f9fee86d53097
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-06-04 09:09:39 +0200

    added coinflip backend

---
+++ backend/src/main/java/de/szut/lf8_starter/game/games/coinFlip/CoinFlipGame.java
@@ -0,0 +1,39 @@
+package de.szut.lf8_starter.game.games.coinFlip;
+
+import de.szut.lf8_starter.game.BaseGame;
+import de.szut.lf8_starter.game.HeaderedText;
+
+public class CoinFlipGame extends BaseGame {
+    @Override
+    public String getId() {
+        return "0b283c6d-b147-4d66-94a4-e120811d1cf0";
+    }
+
+    @Override
+    public String getTitle() {
+        return "Coin Flip";
+    }
+
+    @Override
+    public HeaderedText[] getDescription() { return new HeaderedText[0]; }
+
+    @Override
+    public String getLovelyDescriptionMessage() {
+        return "TBA";
+    }
+
+    @Override
+    public String getPreviewImageUrl() {
+        return "https://cdn.dribbble.com/userupload/20657419/file/original-bb95adb64824bcad48fca0e4f568c23e.gif";
+    }
+
+    @Override
+    public String getLoadingIconUrl() {
+        return "/images/cazinomate-logo.svg";
+    }
+
+    @Override
+    public String getPreviewHexColor() {
+        return "#2A03C7";
+    }
+}
---
+++ backend/src/main/java/de/szut/lf8_starter/game/games/coinFlip/CoinFlipSessionService.java
@@ -0,0 +1,70 @@
+package de.szut.lf8_starter.game.games.coinFlip;
+
+import de.szut.lf8_starter.game.IGame;
+import de.szut.lf8_starter.game.session.BaseGameSessionService;
+import de.szut.lf8_starter.game.session.BaseSession;
+import de.szut.lf8_starter.game.session.BaseSessionRepository;
+import de.szut.lf8_starter.game.session.enums.GameDifficulty;
+import de.szut.lf8_starter.game.session.enums.GameState;
+import de.szut.lf8_starter.transaction.TransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoinFlipSessionService extends BaseGameSessionService<CoinFlipSessionExtension> {
+    public CoinFlipSessionService(BaseSessionRepository baseSessionRepository,
+                                  CoinFlipSessionExtensionRepository extensionRepository,
+                                  TransactionService transactionService) {
+        super(baseSessionRepository, extensionRepository, transactionService);
+    }
+
+    @Override
+    protected void applyGameLogic(BaseSession baseSession, CoinFlipSessionExtension extension, String interaction) {
+        switch (interaction.toLowerCase()) {
+            case "play" -> {
+                if (hasLost(baseSession.getDifficulty())) {
+                    baseSession.setGameState(GameState.LOST);
+                }
+                else {
+                    baseSession.setGameState(GameState.WON);
+                    addBalanceToUser(baseSession.getUserId(), getPrize(baseSession.getInvestedBalance(), baseSession.getDifficulty()), getGame().getTitle() + " game won");
+                }
+            }
+
+            default -> throw new IllegalStateException("Unexpected interaction: " + interaction);
+        }
+    }
+
+    private int getPrize(int investedBalance, GameDifficulty difficulty) {
+        double factor;
+        switch (difficulty) {
+            case EASY -> factor = 1.5;
+            case NORMAL -> factor = 2;
+            case HARD -> factor = 3;
+            default -> throw new IllegalStateException("Unexpected difficulty: " + difficulty);
+        }
+        return (int)(investedBalance * factor);
+    }
+
+    @Override
+    protected CoinFlipSessionExtension createDefaultExtension(BaseSession baseSession) {
+        var extension = new CoinFlipSessionExtension();
+        extension.setSessionId(baseSession.getId());
+
+        return extension;
+    }
+
+    @Override
+    public IGame getGame() {
+        return new CoinFlipGame();
+    }
+
+    private Boolean hasLost(GameDifficulty difficulty) {
+
+        double r = switch (difficulty) {
+            case EASY -> 0.4;
+            case NORMAL -> 0.51;
+            case HARD -> 0.6;
+        };
+        return Math.random() < r;
+    }
+}
