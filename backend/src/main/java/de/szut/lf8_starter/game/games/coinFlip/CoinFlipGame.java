50bc440a1584409141eb51517b8da6a42839e1b6
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-06-05 10:09:36 +0200

    added description to coinflip

--- backend/src/main/java/de/szut/lf8_starter/game/games/coinFlip/CoinFlipGame.java
+++ backend/src/main/java/de/szut/lf8_starter/game/games/coinFlip/CoinFlipGame.java
@@ -11,29 +11,60 @@ public class CoinFlipGame extends BaseGame {
 
     @Override
     public String getTitle() {
-        return "Coin Flip";
+        return "Flip The Coin";
     }
 
     @Override
-    public HeaderedText[] getDescription() { return new HeaderedText[0]; }
+    public HeaderedText[] getDescription() {
+        return new HeaderedText[] {
+                new HeaderedText("Coins setzen", new String[] {
+                        "Lege fest, wie viele Coins du einsetzen möchtest."
+                }),
+                new HeaderedText("Schwierigkeitsgrad wählen", new String[] {
+                        "Wähle zwischen „Einfach“, „Normal“ (Standard) und „Schwer“.",
+                        "Jede Stufe hat eigene Gewinnchancen und Multiplikatoren."
+                }),
+                new HeaderedText("Spiel starten", new String[] {
+                        "Starte das Spiel mit einem Klick auf „Spiel starten“."
+                }),
+                new HeaderedText("Mit Enter bewegen", new String[] {
+                        "Drücke die Enter-Taste, um dein Hühnchen Schritt für Schritt über die Straße zu steuern."
+                }),
+                new HeaderedText("Spielziel", new String[] {
+                        "Versuche, die richtige Münzseite zu erhalten."
+                }),
+                new HeaderedText("Welche Münzseite", new String[] {
+                        "Gewonnen: Die Münze landet mit dem lebenden Crewmate oben.",
+                        "Verloren: Die Münze zeigt den getöteten Crewmate."
+                }),
+                new HeaderedText("Erneut spielen", new String[] {
+                        "Lust auf eine neue Runde? Du kannst jederzeit wieder einsteigen und dein Glück erneut versuchen."
+                }),
+        };
+    }
 
     @Override
     public String getLovelyDescriptionMessage() {
-        return "TBA";
+        return "Viel Erfolg – und viel Glück beim Flippen!";
     }
 
     @Override
     public String getPreviewImageUrl() {
-        return "https://cdn.dribbble.com/userupload/20657419/file/original-bb95adb64824bcad48fca0e4f568c23e.gif";
+        return "https://cdn.budidev.de/5a7bc";
     }
 
     @Override
     public String getLoadingIconUrl() {
-        return "/images/cazinomate-logo.svg";
+        return "https://cdn.budidev.de/63530";
     }
 
     @Override
     public String getPreviewHexColor() {
-        return "#2A03C7";
+        return "#ADA01E";
+    }
+
+    @Override
+    public boolean getIsPlayable() {
+        return true;
     }
 }
