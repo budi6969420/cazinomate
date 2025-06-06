815df0517c099c5029f4c31dd3d37c1fd9d0a767
Abdul Latif Islambouli <abdullatif.islambouli@thermofisher.com>
2025-06-06 12:02:46 +0200

    added pagination to transactions

--- frontend/src/app/pages/transactions/transactions.component.ts
+++ frontend/src/app/pages/transactions/transactions.component.ts
@@ -1,36 +1,58 @@
 import { Component } from '@angular/core';
-import {Transaction} from "../../models/transaction";
-import {DatePipe, NgClass, NgForOf} from "@angular/common";
-import {UserService} from "../../services/user.service";
+import { Transaction } from "../../models/transaction";
+import { UserService } from "../../services/user.service";
+import { DatePipe, NgClass, NgForOf, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-transactions',
+  standalone: true,
   templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.scss'],
   imports: [
     NgClass,
     NgForOf,
+    NgIf,
     DatePipe
-  ],
-  styleUrls: ['./transactions.component.scss']
+  ]
 })
 export class TransactionsComponent {
-  transactions: Transaction[] = [];
+  allTransactions: Transaction[] = [];
+  displayedTransactions: Transaction[] = [];
 
-  protected groupedTransactions: { [key: string]: Transaction[] } = {};
-  protected groupedMonths: string[] = [];
+  displayedGroupedTransactions: { [key: string]: Transaction[] } = {};
+  displayedGroupedMonths: string[] = [];
 
-  constructor(userService: UserService) {
-    userService.getSelfTransactions().subscribe({
+  pageSize = 20;
+  currentPage = 1;
+  totalPages = 0;
+
+  constructor(private userService: UserService) {
+    this.userService.getSelfTransactions().subscribe({
       next: (transactions) => {
-        if (transactions && transactions.length > 0) {
-          this.groupedTransactions = this.groupTransactionsByMonth(transactions);
-          this.groupedMonths = Object.keys(this.groupedTransactions);
+        if (transactions?.length) {
+          this.allTransactions = transactions.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+          this.totalPages = Math.ceil(this.allTransactions.length / this.pageSize);
+          this.loadPage(1);
         }
       }
     });
   }
 
-   groupTransactionsByMonth(transactions: Transaction[]): { [key: string]: Transaction[] } {
+  loadPage(page: number | string) {
+    page = Number(page);
+    if (page < 1 || page > this.totalPages) return;
+
+    this.currentPage = page;
+
+    const startIndex = (page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+
+    this.displayedTransactions = this.allTransactions.slice(startIndex, endIndex);
+    this.displayedGroupedTransactions = this.groupTransactionsByMonth(this.displayedTransactions);
+    this.displayedGroupedMonths = Object.keys(this.displayedGroupedTransactions);
+  }
+
+  groupTransactionsByMonth(transactions: Transaction[]): { [key: string]: Transaction[] } {
     const grouped: { [key: string]: Transaction[] } = {};
 
     transactions.forEach(tx => {
@@ -45,5 +67,26 @@ export class TransactionsComponent {
     });
 
     return grouped;
+  }
+
+  get compactPageNumbers(): (number | string)[] {
+    const pages: (number | string)[] = [];
+
+    if (this.totalPages <= 7) {
+      return Array(this.totalPages).fill(0).map((_, i) => i + 1);
+    }
+
+    const nearStart = this.currentPage <= 4;
+    const nearEnd = this.currentPage >= this.totalPages - 3;
+
+    if (nearStart) {
+      pages.push(1, 2, 3, 4, 5, '...', this.totalPages);
+    } else if (nearEnd) {
+      pages.push(1, '...', this.totalPages - 4, this.totalPages - 3, this.totalPages - 2, this.totalPages - 1, this.totalPages);
+    } else {
+      pages.push(1, '...', this.currentPage - 1, this.currentPage, this.currentPage + 1, '...', this.totalPages);
+    }
+
+    return pages;
   }
 }
